725f3a59e450e10ce29b847affb8306f2b4318c5
Densodim <25denso@gmail.com>
2024-06-17 00:36:53 +0300

    add HW-1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,19 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+type UserMessageType = {
+    avatar: string
+    name: string
+}
+type ArrayMessageType = {
+    text:string
+    time: string
+}
+export type MessageType = {
+    id: number
+    user:UserMessageType
+    message: ArrayMessageType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
