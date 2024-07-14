55ad1634a94d3e0f8885bac7136da8312efc0d74
Densodim <25denso@gmail.com>
2024-07-15 00:28:54 +0300

    add HW-5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -10,8 +10,7 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+    const sidebarClass = `${s.sidebar} ${open ? s.open : ''}`;
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +30,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => (isActive ? `${s.navLink} ${s.active}` : s.navLink)} // делает студент
+                        className={({ isActive }) => (isActive ?  `${s.navLink} ${s.active}` : s.navLink)} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +38,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({ isActive }) => (isActive ? `${s.navLink} ${s.active}` : s.navLink)} // делает студент
+                        className={({ isActive }) => (isActive ?  `${s.navLink} ${s.active}` : s.navLink)} // делает студент
                     >
                         Junior
                     </NavLink>
