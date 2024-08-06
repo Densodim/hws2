2d5687b938b1743b30b6b1eb9c2ce94eb1cb4ad7
Densodim <25denso@gmail.com>
2024-08-06 16:13:15 +0300

    add HW-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const copyState = [...state];
+            const direction = action.payload === 'up' ? 1 : -1;
+            return copyState.sort((a, b) => (
+                a.name > b.name ? direction : -direction
+            ))
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)
         }
         default:
             return state
